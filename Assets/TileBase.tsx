9dc64a2a0641de52b7296c814115a00e220ddaf4
Allan David de Oliveira Amorim <allan.j.k15@gmail.com>
2021-05-30 21:19:38 -0300

    Initial commit

---
+++ Assets/TileBase.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="TileBase" tilewidth="32" tileheight="32" tilecount="36" columns="6">
+ <image source="TilesBase.png" width="192" height="192"/>
+</tileset>
